e3c9bb139b95e7d260f3570574579a31acb0d74a
Misky Abshir <Misky.Abshir@gmail.com>
2019-09-02 09:44:42 -0500

    Last commit before watching live solve

--- client.js
+++ client.js
@@ -2,7 +2,6 @@
 // first name, last name, ID number, job title, annual salary_.
 
 let EmployeeArray = [];
-let MonthlyTotal = 0;
 
 $(document).ready(readyNow);
 
@@ -24,15 +23,16 @@ function appendDom(){
 
     $('.container').append(`<input type="text" placeholder ="First Name" id = "firstName">`)
     $('.container').append(`<input type="text" placeholder ="Last Name" id = "lastName">`)
-    $('.container').append(`<input type="text" placeholder ="ID" id = "ID">`)
+    $('.container').append(`<input type="number" placeholder ="ID" id = "ID">`)
     $('.container').append(`<input type="text" placeholder ="Title" id = "Title">`)
-    $('.container').append(`<input type="text" placeholder ="Annual Salary" id = "EmployeeSalary">`)
+    $('.container').append(`<input type="number" placeholder ="Annual Salary" id = "EmployeeSalary">`)
     $('.container').append(`<button id = "submitBtn">Submit</button>`)
-
 
 
     let tableName = $(`<h3>Employees</h3>`)
     $('.container').append(tableName);
+    
+
 
     let table = $(`<table></table>`);
     table.append('<thead><tr><th id = "FirstName">First Name</th><th id = "LastName">Last Name</th><th id = "ID">ID</th><th id = "Title">Title</th><th id = "Salary">Annual Salary</th><th></th></thead>')
@@ -40,6 +40,11 @@ function appendDom(){
     let tbody = $('<tbody id="tableBody"></tbody>');
     table.append(tbody);
     $('.container').append(table);
+
+    let total = $(`<h3 id ="h3">Monthly Total: $<span id = "TotalEmpSalary"></span> </h3>`)
+
+    $('.container').append(total);
+
 }
 
 function handlesubmitBtn(){
@@ -59,6 +64,7 @@ function handlesubmitBtn(){
     EmployeeArray.push(object);
     PrintResult();
 
+
      $('#firstName').val('');
      $('#lastName').val('');
      $('#ID').val('');
@@ -69,13 +75,18 @@ function handlesubmitBtn(){
 
 function PrintResult(){
     $('#tableBody').empty();
-    EmployeeArray.forEach(function(person){
-        let $listItem = $(`<tr><td>`+ person.FirstName + `</td>`+`<td>` + person.LastName +`</td>` + `<td>` + person.Employee_ID + `</td>` + `<td>` + person.Employee_Title +`</td>` + `<td>`+ person.Employee_AnnualSalary + `<td>` +`<button id = "DeleteButn">Delete</button)`+`</td>`+`</tr></td>`)
-        $('#tableBody').append($listItem);
+    let MonthlyTotal = 0;
 
+    EmployeeArray.forEach(function(person){
+        let listItem = $(`<tr class = "Cell" ><td>`+ person.FirstName + `</td>`+`<td>` + person.LastName +`</td>` + `<td>` + person.Employee_ID + `</td>` + `<td>` + person.Employee_Title +`</td>` + `<td id ="salary">`+ person.Employee_AnnualSalary + `<td>` +`<button id = "DeleteButn">Delete</button)`+`</td>`+`</tr></td>`)
+        $('#tableBody').append(listItem)
+        
+        MonthlyTotal+=Number(person.Employee_AnnualSalary);
     })
+    $('#TotalEmpSalary').append(MonthlyTotal/12);
+
 }
 
 function handleDeleteButn(){
-    console.log('hello');
+    $(this).closest('tr').remove();
 }
